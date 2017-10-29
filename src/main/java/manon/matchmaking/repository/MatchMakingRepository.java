4d06cd2ed49ee2b194b9889765e24330c1b1dc95
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2017-10-29 17:10:56 +0100

    (feat) code cleanup

--- src/main/java/manon/matchmaking/repository/MatchMakingRepository.java
+++
@@ -1,7 +0,0 @@
-package manon.matchmaking.repository;
-
-import manon.matchmaking.document.MatchMaking;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MatchMakingRepository extends MongoRepository<MatchMaking, String> {
-}
