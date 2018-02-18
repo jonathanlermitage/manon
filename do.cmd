cf79b9c1f0a7eee7ffcd8a1fd0b1e05e11f1de75
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2018-02-18 18:54:31 +0100

    (lib) migration: Maven to Gradle

--- do.cmd
+++ do.cmd
@@ -13,38 +13,36 @@ if [%1] == [help] (
 )
 
 if [%1] == [t] ( 
-  echo mvnw clean test
-  mvnw clean test
+  echo gradlew clean test
+  gradlew clean test
 )
 if [%1] == [tc] (
-  echo mvnw clean test -Pcoverage
-  mvnw clean test -Pcoverage
+  echo gradlew clean test jacocoTestReport -Pcoverage=true
+  gradlew clean test jacocoTestReport -Pcoverage=true
 )
 if [%1] == [b] (
-  echo mvnw clean compile -DskipTests -T1
-  mvnw clean compile -DskipTests -T1
+  echo gradlew clean compileJava compileTestJava -x test
+  gradlew clean compileJava compileTestJava -x test
 )
 if [%1] == [c] (
-  echo mvnw clean
-  mvnw clean 
+  echo gradlew clean
+  gradlew clean 
 )
 if [%1] == [p] ( 
-  echo mvnw clean package -DskipTests -T1
-  mvnw clean package -DskipTests -T1 
+  echo gradlew clean uberJar -x test
+  gradlew clean uberJar -x test
 )
 if [%1] == [w] ( 
-  echo mvn -N io.takari:maven:wrapper -Dmaven=%2
-  mvn -N io.takari:maven:wrapper -Dmaven=%2
+  echo gradle wrapper --gradle-version=%2
+  gradle wrapper --gradle-version=%2
 )
 if [%1] == [cv] (
-  echo mvnw versions:display-plugin-updates versions:display-dependency-updates -U
-  mvnw versions:display-plugin-updates versions:display-dependency-updates -U
+  echo gradlew dependencyUpdates -Drevision=release -DoutputFormatter=plain -DoutputDir=./build/
+  gradlew dependencyUpdates -Drevision=release -DoutputFormatter=plain -DoutputDir=./build/
 )
 if [%1] == [uv] (
-  echo mvnw versions:update-properties -U
-  mvnw versions:update-properties -U
+  echo TODO
 )
 if [%1] == [dt] (
-  echo mvnw dependency:tree
-  mvnw dependency:tree
+  echo TODO
 )
