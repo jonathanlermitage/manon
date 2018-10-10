92d04e6be44fc4ee62dc23c60abba70f61feb30f
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2018-10-10 19:57:40 +0200

    (feat) improve stats readability by adding a footer

--- src/test/java/manon/app/stats/service/PerformanceRecorderTest.java
+++ src/test/java/manon/app/stats/service/PerformanceRecorderTest.java
@@ -8,8 +8,8 @@ import static org.testng.Assert.assertTrue;
 
 public class PerformanceRecorderTest {
     
-    private static final String STATS_HEADER = "Services performance (ms):\n" +
-        " calls     min     max     total     avg  median  name";
+    private static final String STATS_TITLE = "Services performance (ms):";
+    private static final String STATS_HEADER = " calls     min     max     total     avg  median  name";
     
     @Test
     public void shouldEmptyShowStats() {
@@ -31,9 +31,10 @@ public class PerformanceRecorderTest {
         performanceRecorder.saveTime("manon.user.UserService:do2()", 200);
         
         assertFalse(performanceRecorder.isEmpty());
-        assertEquals(performanceRecorder.showStats(), STATS_HEADER + "\n" +
+        assertEquals(performanceRecorder.showStats(), STATS_TITLE + "\n" + STATS_HEADER + "\n" +
             "     3       1       2         5       1       2  m.user.UserService:do1()\n" +
-            "     4     100     300       800     200     200  m.user.UserService:do2()");
+            "     4     100     300       800     200     200  m.user.UserService:do2()\n" +
+            STATS_HEADER);
     }
     
     @Test
@@ -44,9 +45,10 @@ public class PerformanceRecorderTest {
         performanceRecorder.saveTime("manon.user.UserService:do2()", 100);
         
         assertFalse(performanceRecorder.isEmpty());
-        assertEquals(performanceRecorder.showStats(), STATS_HEADER + "\n" +
+        assertEquals(performanceRecorder.showStats(), STATS_TITLE + "\n" + STATS_HEADER + "\n" +
             "     1       1       1         1       1       1  m.user.UserService:do1()\n" +
-            "     1     100     100       100     100     100  m.user.UserService:do2()");
+            "     1     100     100       100     100     100  m.user.UserService:do2()\n" +
+            STATS_HEADER);
         
         performanceRecorder.saveTime("manon.user.UserService:do1()", 2);
         performanceRecorder.saveTime("manon.user.UserService:do1()", 2000);
@@ -55,8 +57,9 @@ public class PerformanceRecorderTest {
         performanceRecorder.saveTime("manon.user.UserService:do2()", 200);
         
         assertFalse(performanceRecorder.isEmpty());
-        assertEquals(performanceRecorder.showStats(), STATS_HEADER + "\n" +
+        assertEquals(performanceRecorder.showStats(), STATS_TITLE + "\n" + STATS_HEADER + "\n" +
             "     4     100     300       800     200     200  m.user.UserService:do2()\n" +
-            "     3       1    2000      2003     667       2  m.user.UserService:do1()");
+            "     3       1    2000      2003     667       2  m.user.UserService:do1()\n" +
+            STATS_HEADER);
     }
 }
