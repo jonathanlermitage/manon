bad449c54e4617893f63a1760f7624ef8a179d39
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2018-11-15 13:58:47 +0100

    (refac) remove unnecessary echo in do.cmd and do.sh

--- do.cmd
+++ do.cmd
@@ -18,65 +18,47 @@ if [%1] == [help] (
 )
 
 if [%1] == [t] (
-  echo mvnw clean test
   mvnw clean test -Pembed-win
 )
 if [%1] == [tc] (
-  echo mvnw clean test -Pcoverage
   mvnw clean test -Pembed-win,coverage
 )
 if [%1] == [b] (
-  echo mvnw clean compile -DskipTests -T1
   mvnw clean compile -DskipTests -T1
 )
 if [%1] == [c] (
-  echo mvnw clean
   mvnw clean
 )
 if [%1] == [p] (
-  echo mvnw clean package -DskipTests -T1
   mvnw clean package -DskipTests -T1
 )
 if [%1] == [rd] (
-  echo build project: mvnw clean package -DskipTests -T1
   mvnw clean package -DskipTests -T1
-  echo move to app: cd target
   cd target
-  echo run app in dev mode: java -jar -Xms128m -Xmx512m -Dspring.profiles.active=dev,metrics -Dfile.encoding=UTF-8 -Djava.awt.headless=true -XX:CompileThreshold=1500 manon.jar
   java -jar -Xms128m -Xmx512m -Dspring.profiles.active=dev,metrics -Dfile.encoding=UTF-8 -Djava.awt.headless=true -XX:CompileThreshold=1500 manon.jar
-  echo application exit
-  echo return to root: cd ..
   cd ..
 )
 if [%1] == [w] (
-  echo mvn -N io.takari:maven:wrapper -Dmaven=%2
   mvn -N io.takari:maven:wrapper -Dmaven=%2
 )
 if [%1] == [cv] (
-  echo mvnw versions:display-property-updates -U -P coverage,jib
   mvnw versions:display-property-updates -U -P coverage,jib,embed-win
 )
 if [%1] == [uv] (
-  echo mvnw versions:update-properties -U -P coverage,jib
   mvnw versions:update-properties -U -P coverage,jib,embed-win
 )
 if [%1] == [dt] (
-  echo mvnw dependency:tree
   mvnw dependency:tree
 )
 if [%1] == [sc] (
-  echo mvnw sonar:sonar -Dsonar.organization=%TK1_MANON_SONAR_ORGA% -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=%TK1_MANON_SONAR_LOGIN%
   mvnw sonar:sonar -Dsonar.organization=%TK1_MANON_SONAR_ORGA% -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=%TK1_MANON_SONAR_LOGIN%
 )
 if [%1] == [tsc] (
-  echo mvnw clean test sonar:sonar -Pcoverage -Dsonar.organization=%TK1_MANON_SONAR_ORGA% -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=%TK1_MANON_SONAR_LOGIN%
   mvnw clean test sonar:sonar -Pembed-win,coverage -Dsonar.organization=%TK1_MANON_SONAR_ORGA% -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=%TK1_MANON_SONAR_LOGIN%
 )
 if [%1] == [jib] (
-  echo mvnw clean compile jib:dockerBuild -DskipTests -P jib
   mvnw clean compile jib:dockerBuild -DskipTests -P jib
 )
 if [%1] == [jibtar] (
-  echo mvnw clean compile jib:buildTar -DskipTests -P jib
   mvnw clean compile jib:buildTar -DskipTests -P jib
 )
--- do.sh
+++ do.sh
@@ -23,102 +23,77 @@ case "$1" in
   ;;
 
 "t")
-  echo "sh ./mvnw clean test"
   sh ./mvnw clean test -Pembed-linux
   ;;
 
 "tc")
-  echo "sh ./mvnw clean test -Pcoverage"
   sh ./mvnw clean test -Pembed-linux,coverage
   ;;
 
 "b")
-  echo "sh ./mvnw clean compile -DskipTests -T1"
   sh ./mvnw clean compile -DskipTests -T1
   ;;
 
 "c")
-  echo "sh ./mvnw clean"
   sh ./mvnw clean
   ;;
 
 "p")
-  echo "sh ./mvnw clean package -DskipTests -T1"
   sh ./mvnw clean package -DskipTests -T1
   ;;
 
 "rd")
-  echo "build project: sh ./mvnw clean package -DskipTests -T1"
   sh ./mvnw clean package -DskipTests -T1
-  echo "move to app: cd target/"
   cd target/
-  echo "run app in dev mode: java -jar -Xms128m -Xmx512m -Dspring.profiles.active=dev,metrics -Dfile.encoding=UTF-8 -Djava.awt.headless=true -XX:CompileThreshold=1500 manon.jar"
   java -jar -Xms128m -Xmx512m -Dspring.profiles.active=dev,metrics -Dfile.encoding=UTF-8 -Djava.awt.headless=true -XX:CompileThreshold=1500 manon.jar
-  echo "application exit"
-  echo "return to root: cd .."
   cd ..
   ;;
 
 "w")
-  echo "mvn -N io.takari:maven:wrapper -Dmaven=\$2"
   mvn -N io.takari:maven:wrapper -Dmaven=$2
   ;;
 
 "cv")
-  echo "sh ./mvnw versions:display-property-updates -U -P coverage,jib"
   sh ./mvnw versions:display-property-updates -U -P coverage,jib,embed-linux
   ;;
 
 "uv")
-  echo "sh ./mvnw versions:update-properties -U -P coverage,jib"
   sh ./mvnw versions:update-properties -U -P coverage,jib,embed-linux
   ;;
 
 "dt")
-  echo "sh ./mvnw dependency:tree"
   sh ./mvnw dependency:tree
   ;;
 
 "sc")
-  echo "sh ./mvnw sonar:sonar -Dsonar.organization=\$TK1_MANON_SONAR_ORGA -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=\$TK1_MANON_SONAR_LOGIN"
   sh ./mvnw sonar:sonar -Dsonar.organization=$TK1_MANON_SONAR_ORGA -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$TK1_MANON_SONAR_LOGIN
   ;;
 
 "tsc")
-  echo "sh ./mvnw clean test sonar:sonar -Pcoverage -Dsonar.organization=\$TK1_MANON_SONAR_ORGA -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=\$TK1_MANON_SONAR_LOGIN"
   sh ./mvnw clean test sonar:sonar -Pembed-linux,coverage -Dsonar.organization=$TK1_MANON_SONAR_ORGA -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$TK1_MANON_SONAR_LOGIN
   ;;
 
 "rmi")
-  echo "docker-compose stop"
   docker-compose stop
-  echo "rm \$(docker ps -a | grep \"lermitage-manon\" | awk '{print \$1}')"
   docker rm $(docker ps -a | grep "lermitage-manon" | awk '{print $1}')
-  echo "docker rmi \$(docker images | grep \"^lermitage-manon\" | awk '{print \$3}')"
   docker rmi $(docker images | grep "^lermitage-manon" | awk '{print $3}')
-  echo "docker rmi \$(docker images | grep \"gcr.io/distroless/java\" | awk '{print \$3}')"
   docker rmi $(docker images | grep "gcr.io/distroless/java" | awk '{print $3}')
   ;;
 
 "cdi")
-  echo "rmi \$(docker images -f \"dangling=true\" -q)"
   docker rmi $(docker images -f "dangling=true" -q)
   ;;
 
 "docker")
-  echo "sh ./mvnw clean package -DskipTests -T1"
   sh ./mvnw clean package -DskipTests -T1
-  echo "docker build -t lermitage-manon:1.0.0-SNAPSHOT ."
   docker build -t lermitage-manon:1.0.0-SNAPSHOT .
   ;;
 
 "jib")
-  echo "sh ./mvnw clean compile jib:dockerBuild -DskipTests -P jib"
   sh ./mvnw clean compile jib:dockerBuild -DskipTests -P jib
   ;;
 
 "jibtar")
-  echo "sh ./mvnw clean compile jib:buildTar -DskipTests -P jib"
   sh ./mvnw clean compile jib:buildTar -DskipTests -P jib
   ;;
 
