1826303c2854327fcaa3df6eb5602327610c3d1f
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2019-01-13 02:04:38 +0100

    (test) introduce archunit tests

---
+++ src/test/java/manon/util/ArchTest.java
@@ -0,0 +1,80 @@
+package manon.util;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.testng.annotations.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+public class ArchTest {
+    
+    private final String[] controlerPackages = new String[]{"manon.app.batch.api", "manon.app.info.api", "manon.user.api"};
+    private final String[] repositoryPackages = new String[]{"manon.app.trace.repository", "manon.user.repository"};
+    private final String[] servicePackages = new String[]{"manon.app.trace.service", "manon.user.service"};
+    
+    @Test
+    public void shouldVerifyControlerArch() {
+        JavaClasses classes = new ClassFileImporter().importPackages(controlerPackages);
+        ArchRule rules = classes().that()
+            .haveSimpleNameEndingWith("WS")
+            .should().beAnnotatedWith(RestController.class)
+            .andShould().beAnnotatedWith(RequestMapping.class)
+            .andShould().notBeInterfaces();
+        rules.check(classes);
+    }
+    
+    @Test
+    public void shouldVerifyRepositoryArch() {
+        JavaClasses classes = new ClassFileImporter().importPackages(repositoryPackages);
+        ArchRule rules = classes().that()
+            .haveSimpleNameEndingWith("Repository")
+            .should().beAnnotatedWith(Repository.class)
+            .andShould().beInterfaces();
+        rules.check(classes);
+    }
+    
+    @Test
+    public void shouldVerifyRepositoryCustomArch() {
+        JavaClasses classes = new ClassFileImporter().importPackages(repositoryPackages);
+        ArchRule rules = classes().that()
+            .haveSimpleNameEndingWith("RepositoryCustom")
+            .should().notBeAnnotatedWith(Repository.class)
+            .andShould().beInterfaces();
+        rules.check(classes);
+    }
+    
+    @Test
+    public void shouldVerifyRepositoryImplArch() {
+        JavaClasses classes = new ClassFileImporter().importPackages(repositoryPackages);
+        ArchRule rules = classes().that()
+            .haveSimpleNameEndingWith("RepositoryImpl")
+            .should().beAnnotatedWith(Repository.class)
+            .andShould().notBeInterfaces();
+        rules.check(classes);
+    }
+    
+    @Test
+    public void shouldVerifyServiceArch() {
+        JavaClasses classes = new ClassFileImporter().importPackages(servicePackages);
+        ArchRule rules = classes().that()
+            .haveSimpleNameEndingWith("Service")
+            .should().notBeAnnotatedWith(Service.class)
+            .andShould().beInterfaces();
+        rules.check(classes);
+    }
+    
+    @Test
+    public void shouldVerifyServicesImplArch() {
+        JavaClasses classes = new ClassFileImporter().importPackages(servicePackages);
+        ArchRule rules = classes().that()
+            .haveSimpleNameEndingWith("ServiceImpl")
+            .should().beAnnotatedWith(Service.class)
+            .andShould().notBeInterfaces();
+        rules.check(classes);
+    }
+}
