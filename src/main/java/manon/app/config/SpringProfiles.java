fdf0b885c911a9f202c4aa209fbd69bbe5cfa7a8
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2019-02-15 19:19:07 +0100

    (refac) remove unused code

--- src/main/java/manon/app/config/SpringProfiles.java
+++ src/main/java/manon/app/config/SpringProfiles.java
@@ -10,7 +10,4 @@ public final class SpringProfiles {
     
     /** {@value}, an <b>additional profile</b> to enable performance metrics collection. */
     public static final String METRICS = "metrics";
-    
-    /** {@value}. */
-    public static final String NOT_METRICS = "!metrics";
 }
