0ea20517c190e946b2a51087687bb972efe5d33f
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2019-02-25 19:49:30 +0100

    (feat) make JSR-310 dates human-readable when written to JSON

--- src/main/java/manon/app/config/ConverterConfig.java
+++ src/main/java/manon/app/config/ConverterConfig.java
@@ -1,6 +1,7 @@
 package manon.app.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
@@ -17,7 +18,9 @@ public class ConverterConfig implements WebMvcConfigurer {
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.stream().filter(converter -> converter instanceof AbstractJackson2HttpMessageConverter).forEach(converter -> {
             ObjectMapper objectMapper = ((AbstractJackson2HttpMessageConverter) converter).getObjectMapper();
-            objectMapper.setSerializationInclusion(NON_NULL);
+            objectMapper
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .setSerializationInclusion(NON_NULL);
         });
     }
 }
