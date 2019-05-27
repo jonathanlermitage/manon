46d49dc44ea3c69a02b51ebf1d8d5ca506443aef
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2019-05-27 17:27:30 +0200

    (refac) remove unused method

--- src/main/java/manon/repository/user/UserSnapshotRepository.java
+++ src/main/java/manon/repository/user/UserSnapshotRepository.java
@@ -5,14 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Repository
 public interface UserSnapshotRepository extends JpaRepository<UserSnapshot, Long> {
     
     long countAllByCreationDateBetween(LocalDateTime start, LocalDateTime end);
-    
-    List<UserSnapshot> findAllByUserId(long userId);
     
     void deleteAllByCreationDateBetween(LocalDateTime start, LocalDateTime end);
     
