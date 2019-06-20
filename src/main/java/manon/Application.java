bc215a521e4c26489987615f90c67ac49c0a48e3
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2019-06-21 01:52:05 +0200

    (feat) write application's PID

--- src/main/java/manon/Application.java
+++ src/main/java/manon/Application.java
@@ -5,11 +5,11 @@ import manon.app.Cfg;
 import manon.service.app.AppTraceService;
 import manon.service.user.RegistrationService;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.retry.annotation.EnableRetry;
@@ -36,7 +36,9 @@ public class Application extends SpringBootServletInitializer {
     private final RegistrationService registrationService;
     
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplicationBuilder app = new SpringApplicationBuilder(Application.class);
+        app.build().addListeners(new ApplicationPidFileWriter());
+        app.run(args);
     }
     
     @Override
