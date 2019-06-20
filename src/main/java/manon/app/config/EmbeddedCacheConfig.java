dc0b80d66364be6de461f561f67133170c262818
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2019-06-20 18:31:38 +0200

    (feat) enable Redis or in-memory cache, and cache JWT references

---
+++ src/main/java/manon/app/config/EmbeddedCacheConfig.java
@@ -0,0 +1,26 @@
+package manon.app.config;
+
+import manon.app.Globals;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static manon.app.Globals.Properties.CACHE_PROVIDER;
+import static manon.app.Globals.Properties.CACHE_PROVIDER_EMBEDDED;
+
+@Configuration
+@ConditionalOnProperty(name = CACHE_PROVIDER, havingValue = CACHE_PROVIDER_EMBEDDED)
+@EnableCaching(proxyTargetClass = true)
+public class EmbeddedCacheConfig extends CachingConfigurerSupport {
+    
+    @Bean
+    public CacheManager cacheManager() {
+        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
+        cacheManager.setCacheNames(Globals.CacheNames.ALL_CACHES());
+        return cacheManager;
+    }
+}
---
+++ src/main/java/manon/app/config/RedisCacheConfig.java
@@ -0,0 +1,43 @@
+package manon.app.config;
+
+import lombok.RequiredArgsConstructor;
+import manon.app.Cfg;
+import manon.app.Globals;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.Set;
+
+import static manon.app.Globals.Properties.CACHE_PROVIDER;
+import static manon.app.Globals.Properties.CACHE_PROVIDER_REDIS;
+
+@Configuration
+@EnableCaching
+@ConditionalOnProperty(name = CACHE_PROVIDER, havingValue = CACHE_PROVIDER_REDIS)
+@RequiredArgsConstructor
+public class RedisCacheConfig extends CachingConfigurerSupport {
+    
+    private final Cfg cfg;
+    
+    @SuppressWarnings({"CollectionAddAllCanBeReplacedWithConstructor", "ConstantConditions"})
+    @Bean
+    public RedisCacheManager cacheManager(RedisTemplate redisTemplate) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofSeconds(cfg.getCacheRedisTtl()))
+            .prefixKeysWith("manon_");
+        Set<String> cacheNames = new HashSet<>();
+        cacheNames.addAll(Globals.CacheNames.ALL_CACHES());
+        return RedisCacheManager.builder(redisTemplate.getConnectionFactory())
+            .cacheDefaults(config)
+            .initialCacheNames(cacheNames)
+            .build();
+    }
+}
