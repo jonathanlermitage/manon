2f3037b6abb61c1727a74049b664b27504afe53a
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2019-12-23 17:10:47 +0100

    (test) improve architecture tests
    See this excellent article: https://blog.scottlogic.com/2019/12/05/unit-test-your-architecture-with-archunit.html

--- src/test/java/manon/util/ArchTest.java
+++ src/test/java/manon/util/ArchTest.java
@@ -16,13 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 public class ArchTest extends AbstractParallelTest {
-    
+
     private static final JavaClasses PROJECT = new ClassFileImporter()
+        .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_JARS)
         .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
         .importPackages("manon..");
-    
+
     private static final ArchCondition<JavaClass> NOT_CALL_METHOD_THAT_EXISTS_FOR_TESTS =
         new ArchCondition<JavaClass>("not call methods that exist for tests") {
             @Override
@@ -38,7 +40,7 @@ public class ArchTest extends AbstractParallelTest {
                     });
             }
         };
-    
+
     private static final ArchCondition<JavaClass> NOT_CALL_OTHER_SERVICE_REPOSITORY =
         new ArchCondition<JavaClass>("service should not depend on other services repositories (fooService should depend on fooRepository, not barRepository)") {
             @Override
@@ -57,21 +59,35 @@ public class ArchTest extends AbstractParallelTest {
                 }
             }
         };
-    
+
     @Test
     public void shouldNotDependOnJDKInternals() {
         classes()
             .should().onlyAccessClassesThat().resideOutsideOfPackages("com.sun..", "sun..")
             .check(PROJECT);
     }
-    
+
     @Test
     public void shouldVerifyProductionCodeDontUseMethodThatExistsForTests() {
         classes()
             .should(NOT_CALL_METHOD_THAT_EXISTS_FOR_TESTS)
             .check(PROJECT);
     }
-    
+
+    @Test
+    public void shouldVerifyLayeredArchitecture() {
+        // a professional project would also define layers like Entity and Dto, then ensure strong architecture
+        layeredArchitecture()
+            .layer("Config").definedBy("manon.app.config..", "manon")
+            .layer("API").definedBy("..api..")
+            .layer("Batch").definedBy("..batch..")
+            .layer("Repository").definedBy("..repository..")
+            .layer("Service").definedBy("..service..")
+            .whereLayer("Repository").mayOnlyBeAccessedByLayers("Batch", "Service")
+            .whereLayer("Service").mayOnlyBeAccessedByLayers("API", "Batch", "Config")
+            .check(PROJECT);
+    }
+
     @Test
     public void shouldVerifyServiceDependsOnItsOwnRepository() {
         classes().that()
@@ -79,7 +95,7 @@ public class ArchTest extends AbstractParallelTest {
             .should(NOT_CALL_OTHER_SERVICE_REPOSITORY)
             .check(PROJECT);
     }
-    
+
     @Test
     public void shouldVerifyControlerArch() {
         classes().that()
@@ -87,18 +103,20 @@ public class ArchTest extends AbstractParallelTest {
             .should().beAnnotatedWith(RestController.class)
             .andShould().beAnnotatedWith(RequestMapping.class)
             .andShould().notBeInterfaces()
+            .andShould().resideInAPackage("..api..")
             .check(PROJECT);
     }
-    
+
     @Test
     public void shouldVerifyRepositoryArch() {
         classes().that()
             .haveSimpleNameEndingWith("Repository")
             .should().beAnnotatedWith(Repository.class)
             .andShould().beInterfaces()
+            .andShould().resideInAPackage("..repository..")
             .check(PROJECT);
     }
-    
+
     @Test
     public void shouldVerifyRepositoryAreUsedByServicesOnly() {
         classes().that()
@@ -106,40 +124,44 @@ public class ArchTest extends AbstractParallelTest {
             .should().onlyBeAccessed().byClassesThat().areAnnotatedWith(Service.class)
             .check(PROJECT);
     }
-    
+
     @Test
     public void shouldVerifyRepositoryCustomArch() {
         classes().that()
             .haveSimpleNameEndingWith("RepositoryCustom")
             .should().notBeAnnotatedWith(Repository.class)
+            .andShould().resideInAPackage("..repository..")
             .andShould().beInterfaces()
             .check(PROJECT);
     }
-    
+
     @Test
     public void shouldVerifyRepositoryImplArch() {
         classes().that()
             .haveSimpleNameEndingWith("RepositoryImpl")
             .should().beAnnotatedWith(Repository.class)
             .andShould().notBeInterfaces()
+            .andShould().resideInAPackage("..repository..")
             .check(PROJECT);
     }
-    
+
     @Test
     public void shouldVerifyServiceArch() {
         classes().that()
             .haveSimpleNameEndingWith("Service")
             .should().notBeAnnotatedWith(Service.class)
             .andShould().beInterfaces()
+            .andShould().resideInAPackage("..service..")
             .check(PROJECT);
     }
-    
+
     @Test
     public void shouldVerifyServicesImplArch() {
         classes().that()
             .haveSimpleNameEndingWith("ServiceImpl")
             .should().beAnnotatedWith(Service.class)
             .andShould().notBeInterfaces()
+            .andShould().resideInAPackage("..service..")
             .check(PROJECT);
     }
 }
