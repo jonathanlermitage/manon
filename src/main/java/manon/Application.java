df4565443b4d5cd96c0c8b4facbfb35f6db3cdb5
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2019-12-24 13:38:10 +0100

    (feat) log auto-configuration

--- src/main/java/manon/Application.java
+++ src/main/java/manon/Application.java
@@ -11,12 +11,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableAsync
@@ -35,7 +37,13 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplicationBuilder app = new SpringApplicationBuilder(Application.class);
         app.build().addListeners(new ApplicationPidFileWriter());
-        app.run(args);
+        ConfigurableApplicationContext ctx = app.run(args);
+        if (log.isDebugEnabled()) {
+            Stream.of(ctx.getBeanDefinitionNames())
+                .filter(s -> s.startsWith("org.springframework") && s.contains(".autoconfigure."))
+                .sorted()
+                .forEach(s -> log.debug("Loaded autoconfig: " + s));
+        }
     }
 
     @Override
