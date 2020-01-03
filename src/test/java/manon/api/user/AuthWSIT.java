20c2497161ccb2ef5ce123dd304c4094f2eecdc0
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2020-01-03 19:35:04 +0100

    (test) ensure auth token can be used several times

--- src/test/java/manon/api/user/AuthWSIT.java
+++ src/test/java/manon/api/user/AuthWSIT.java
@@ -12,7 +12,7 @@ import static org.apache.http.HttpStatus.SC_UNAUTHORIZED;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AuthWSIT extends AbstractIT {
-    
+
     @Test
     public void shouldLogin() {
         Response res = whenAnonymous().getSpec()
@@ -23,11 +23,11 @@ public class AuthWSIT extends AbstractIT {
             .statusCode(SC_OK);
         assertThat(res.asString()).isNotEmpty();
     }
-    
+
     @Test
     public void shouldUseToken() {
         String jwt = loginAndReturnToken(name(1), pwd(1));
-        
+
         Response res = whenAnonymous().getSpec()
             .header("Authorization", "Bearer " + jwt)
             .get(API_USER);
@@ -38,11 +38,28 @@ public class AuthWSIT extends AbstractIT {
         assertThat(webUser).isEqualTo(dbUser);
         assertThat(webUser.getUserSnapshots()).isNull();
     }
-    
+
+    @Test
+    public void shouldReuseToken() {
+        String jwt = loginAndReturnToken(name(1), pwd(1));
+
+        User dbUser = userService.readOne(userId(1)).toBuilder().password(null).build();
+        for (int i = 0; i < 3; i++) {
+            Response res = whenAnonymous().getSpec()
+                .header("Authorization", "Bearer " + jwt)
+                .get(API_USER);
+            res.then()
+                .statusCode(SC_OK);
+            User webUser = readValue(res, User.class);
+            assertThat(webUser).isEqualTo(dbUser);
+            assertThat(webUser.getUserSnapshots()).isNull();
+        }
+    }
+
     @Test
     public void shouldRenewToken() {
         String jwt = loginAndReturnToken(name(1), pwd(1));
-        
+
         Response res = whenAnonymous().getSpec()
             .header("Authorization", "Bearer " + jwt)
             .post(API_USER + "/auth/renew");
@@ -50,7 +67,7 @@ public class AuthWSIT extends AbstractIT {
             .statusCode(SC_OK);
         String newJwt = res.asString();
         assertThat(newJwt).isNotEmpty().isNotEqualTo(jwt);
-        
+
         Response newRes = whenAnonymous().getSpec()
             .header("Authorization", "Bearer " + jwt)
             .get(API_USER);
@@ -61,81 +78,81 @@ public class AuthWSIT extends AbstractIT {
         assertThat(webUser).isEqualTo(dbUser);
         assertThat(webUser.getUserSnapshots()).isNull();
     }
-    
+
     @Test
     public void shouldRejectTokenIfAccountBanned() {
         String jwt = loginAndReturnToken(name(1), pwd(1));
-        
+
         registrationService.ban(userId(1));
-        
+
         whenAnonymous().getSpec()
             .header("Authorization", "Bearer " + jwt)
             .get(API_USER)
             .then()
             .statusCode(SC_UNAUTHORIZED);
     }
-    
+
     @Test
     public void shouldRejectTokenIfAccountDeleted() {
         String jwt = loginAndReturnToken(name(1), pwd(1));
-        
+
         registrationService.delete(userId(1));
-        
+
         whenAnonymous().getSpec()
             .header("Authorization", "Bearer " + jwt)
             .get(API_USER)
             .then()
             .statusCode(SC_UNAUTHORIZED);
     }
-    
+
     @Test
     public void shouldRejectTokenIfAccountSuspended() {
         String jwt = loginAndReturnToken(name(1), pwd(1));
-        
+
         registrationService.suspend(userId(1));
-        
+
         whenAnonymous().getSpec()
             .header("Authorization", "Bearer " + jwt)
             .get(API_USER)
             .then()
             .statusCode(SC_UNAUTHORIZED);
     }
-    
+
     @Test
     public void shouldLogOutAllForCurrentUser() {
         //GIVEN user 1 gets 2 tokens, user 2 get 1 token
         String jwt1a = loginAndReturnToken(name(1), pwd(1));
         String jwt1b = loginAndReturnToken(name(1), pwd(1));
         String jwt2 = loginAndReturnToken(name(2), pwd(2));
-        
+
         //WHEN user 1 invalidates all his tokens
         whenAnonymous().getSpec()
             .header("Authorization", "Bearer " + jwt1a)
             .post(API_USER + "/auth/logout/all")
             .then()
             .statusCode(SC_OK);
-        
+
         //THEN user 1 can't user his first token
         whenAnonymous().getSpec()
             .header("Authorization", "Bearer " + jwt1a)
             .get(API_USER)
             .then()
             .statusCode(SC_UNAUTHORIZED);
-        
+
         //THEN user 1 can't user his second token
         whenAnonymous().getSpec()
             .header("Authorization", "Bearer " + jwt1b)
             .get(API_USER)
             .then()
             .statusCode(SC_UNAUTHORIZED);
-    
+
         //THEN user 2 is not affected by token invalidation asked by user 1
         whenAnonymous().getSpec()
             .header("Authorization", "Bearer " + jwt2)
             .get(API_USER)
             .then()
             .statusCode(SC_OK);
-        
+
         //THEN user 1 can authenticate and use new token
         whenAuthenticated(name(1), pwd(1)).getSpec()
             .header("Authorization", "Bearer " + jwt1b)
